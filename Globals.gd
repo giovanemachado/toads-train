2f30cf42cd79b1da92dad0dc4f518e7ef7ffe575
giovanemachado <giovanemachado3@gmail.com>
2023-07-11 23:13:41 -0300

    adiciona utils assets, corrige os folders, adiciona Globals e SignalBus, tambem configura para exportar para web

---
+++ Globals.gd
@@ -0,0 +1,13 @@
+extends Node
+
+#enum ExampleEnum {A, B}
+#
+#const EXAMPLE_CONST = 'const value'
+#
+#const EXAMPLE_CONST_OBJECT = { 
+#		PROPERTY1 = 'preperty 1', 
+#		PROPERTY2 = 'property 2',
+#}
+
+# To use:
+# Globals.EXAMPLE_CONST
---
+++ SignalBus.gd
@@ -0,0 +1,9 @@
+extends Node
+
+signal game_over
+
+# To connect:
+# SignalBus.game_over.connect(_on_game_over)
+
+# To emit:
+# SignalBus.emit_signal("game_over")
