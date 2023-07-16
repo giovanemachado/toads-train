dcd0a1c164af5789ec1adb9d3c394372e719f0e1
giovanemachado <giovanemachado3@gmail.com>
2023-07-16 09:10:52 -0300

    salva o player como uma scene, e faz as gambiarras necessarias

--- src/gameplay/units/player/Player.gd
+++ src/gameplay/units/player/Player.gd
@@ -5,6 +5,14 @@ extends CharacterBody2D
 @onready var health: Health = $Health
 @onready var interaction: Interaction = $Interaction
 
+@onready var life_bar: ProgressBar = %LifeBar
+
+
+func _ready():
+	life_bar.max_value = health.max_health_points
+	life_bar.value = health.max_health_points
+	
+	
 func _physics_process(delta):
 	var input_vector = Input.get_vector("player_left", "player_right", "player_up", "player_down")
 	movement.movement(input_vector)
@@ -16,3 +24,7 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("player_interact"):
 		interaction.interaction()
+
+
+func _on_health_health_update(old_value, new_value):
+	life_bar.value = new_value
