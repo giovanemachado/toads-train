3137e05f843032e5e068b7475e1e09f02072a334
giovanemachado <giovanemachado3@gmail.com>
2023-07-18 21:43:32 -0300

    ad

--- src/gameplay/units/enemies/test/fsm/EnemyTestStaggerState.gd
+++ src/gameplay/units/enemies/test/fsm/EnemyTestStaggerState.gd
@@ -4,5 +4,8 @@ class_name EnemyTestStaggerState
 
 @onready var state_manager: EnemyTestStateManager = $".."
 
+func _on_enter():
+	state_manager.animation_player.play("hurt")
+
 func _on_health_stagger_recovered():
 	Transitioned.emit(self, "EnemyTestChasingState")
